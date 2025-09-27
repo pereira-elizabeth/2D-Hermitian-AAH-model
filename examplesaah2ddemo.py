ba532c3a057aac33d21868aa5aadebdabcff8960
Elizabeth Louis Pereira <elizabethpereira0331@gmail.com>
2025-09-27 16:25:31 +0300

    Update examplesaah2ddemo.py

--- examplesaah2ddemo.py
+++ examplesaah2ddemo.py
@@ -2,6 +2,8 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import sys, pathlib
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1]))  # add repo root to sys.path
 
 from src.hamiltonian import aah_2d_hamiltonian
 def main():
